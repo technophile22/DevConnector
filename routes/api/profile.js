dc9be4fab9235a34062b8b22eb61716dd3bee142
technophile22 <raghavmangal22@gmail.com>
2021-08-20 17:43:06 +0530

    add and delete education route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -243,4 +243,83 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
 	}
 });
 
+//@route    PUT api/profile/education
+//@desc     add profile education
+//@access   private
+router.put(
+	'/education',
+	[
+		auth,
+		[
+			check('school', 'School is required').not().isEmpty(),
+			check('degree', 'Degree is required').not().isEmpty(),
+			check('fieldofstudy', 'Field of study is required').not().isEmpty(),
+			check('from', 'From date is required').not().isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { school, degree, fieldofstudy, from, to, current, description } =
+			req.body;
+
+		const newEdu = {
+			school,
+			degree,
+			fieldofstudy,
+			from,
+			to,
+			current,
+			description,
+		};
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+
+			//check if profile is there or not
+
+			if (profile) {
+				profile.education.unshift(newEdu);
+				await profile.save();
+
+				return res.json(profile);
+			}
+
+			return res.status(404).json({ msg: 'user not found!' });
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	},
+);
+
+//@route    DELETE api/profile/education/:edu_id
+//@desc     delete education from profile
+//@access   private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id });
+
+		if (profile) {
+			//get remove index
+			const removeIndex = profile.education
+				.map((item) => item.id)
+				.indexOf(req.params.edu_id);
+
+			profile.education.splice(removeIndex, 1);
+
+			await profile.save();
+
+			return res.json(profile);
+		}
+		return res.status(404).json({ msg: 'user not found!' });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
 module.exports = router;
