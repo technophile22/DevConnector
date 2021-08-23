8e53a3222fde8c756a7afb09823f210e0bf85220
technophile22 <raghavmangal22@gmail.com>
2021-08-23 14:17:38 +0530

    like and unlike post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -99,4 +99,74 @@ router.delete('/:id', auth, async (req, res) => {
 	}
 });
 
+//@route    PUT api/posts/like/:id
+//@desc     like a post
+//@access   Private
+router.put('/like/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post) {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+
+		//check if the post has already liked the post
+		if (
+			post.likes.filter((like) => like.user.toString() === req.user.id).length >
+			0
+		) {
+			return res.status(400).json({ msg: 'Post already liked' });
+		}
+
+		//unshift adds to the front instead of back as in push
+		post.likes.unshift({ user: req.user.id });
+
+		await post.save();
+
+		res.json(post.likes);
+	} catch (err) {
+		console.error(err.message);
+		if (err.kind === 'ObjectId') {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+		res.status(500).send('Server Error');
+	}
+});
+
+//@route    PUT api/posts/unlike/:id
+//@desc     unlike a post which is liked
+//@access   Private
+router.put('/unlike/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post) {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+
+		//check if the post has already liked the post
+		if (
+			post.likes.filter((like) => like.user.toString() === req.user.id)
+				.length === 0
+		) {
+			return res.status(400).json({ msg: 'Post has not yet been liked!' });
+		}
+
+		//Get remove index
+		const removeIndex = post.likes
+			.map((like) => like.user.toString())
+			.indexOf(req.user.id);
+
+		post.likes.splice(removeIndex, 1);
+
+		await post.save();
+
+		res.json(post.likes);
+	} catch (err) {
+		console.error(err.message);
+		if (err.kind === 'ObjectId') {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+		res.status(500).send('Server Error');
+	}
+});
+
 module.exports = router;
